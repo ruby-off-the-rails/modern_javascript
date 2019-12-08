20ba5de4847b2f1316a1a36cfecc36b4084432d0
brian <riceb53@gmail.com>
2019-12-08 11:41:39 -0600

    add babel

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 /* global moment */
+var moment = require('moment');
+var math = require('mathjs');
 
 console.log("i am in the js");
 
 
 console.log(moment().format("MMM Do YY"));
 console.log(moment().startOf('hour').fromNow());
+
+
+console.log(' i want to add something');
+
+console.log(math.derivative('x^2 + x', 'x'));
+
+
+var name = "bob";
+var time = "today";
+
+// old syntax
+// console.log("hello" + name + "how are you" + time)
+console.log(`hello ${name}, how are you ${time}`);
